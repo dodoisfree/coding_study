def8bdaf01974c4c74785fe638822cbc086add50
dodoisfree <yocasd2@gmail.com>
2022-01-13 00:38:35 +0900

    제이쿼리

--- lifeCoding/HTML/colors.js
+++ lifeCoding/HTML/colors.js
@@ -1,36 +1,39 @@
-        let Links = {
-            setColor:function(color) {
-            let alist = document.querySelectorAll('a');
-            let i = 0;
-            while (i < alist.length) {
-                alist[i].style.color = 'color';
-                i = i + 1;
-            }
+let Links = {
+    setColor: function (color) {
+        let alist = document.querySelectorAll('a');
+        let i = 0;
+        while (i < alist.length) {
+            alist[i].style.color = 'color';
+            i = i + 1;
         }
-        }
-        let Body = {
-            setColor:function (color) {
-                document.querySelector('body').style.color = color;
-            },
-            setBackgroundColor:function (color) {
-                document.querySelector('body').style.backgroundColor = color;
-            }
-        }
+        //$('a').css('color', color);
+    }
+}
+let Body = {
+    setColor: function (color) {
+        document.querySelector('body').style.color = color;
+        //$('body').css('color', color);
+    },
+    setBackgroundColor: function (color) {
+        document.querySelector('body').style.backgroundColor = color;
+        //$('body').css('backgroundColor', color);
+    }
+}
 
-        function nightDayHandler(self) {
-            let target = document.querySelector('body'); // 변수 사용 
-            if (self.value === 'night') {
-                Body.setBackgroundColor('black');
-                Body.setColor('white');
-                self.value = 'day'
+function nightDayHandler(self) {
+    let target = document.querySelector('body'); // 변수 사용 
+    if (self.value === 'night') {
+        Body.setBackgroundColor('black');
+        Body.setColor('white');
+        self.value = 'day'
 
-                Links.setColor('powderblue');
+        Links.setColor('powderblue');
 
-            } else {
-                Body.setBackgroundColor('white');
-                Body.setColor('black');
-                self.value = 'night';
+    } else {
+        Body.setBackgroundColor('white');
+        Body.setColor('black');
+        self.value = 'night';
 
-                Links.setColor('blue');
-            }
-        }
+        Links.setColor('blue');
+    }
+}
